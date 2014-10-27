424da0bb820ab768833f9c2484b0c3f0f6c8f6e9
Igor Zoriy <gh@igorzoriy.com>
2014-10-27 21:39:54 +0700

    Improve gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,32 @@
 "use strict";
 
-var appPath = 'app';
+var port = 8008;
+var appPath = './app';
 var stylPath = appPath + '/styl/**/*.styl';
 var tplPath = appPath + '/templates';
 var imagesPath = appPath + '/images/*';
-var outputPath = 'output';
+var outputPath = './output';
+
+var del = require('del');
+var fs = require('fs');
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var plumber = require('gulp-plumber');
 var jade = require('gulp-jade');
-var gulpData = require('gulp-data');
-var dataPath = './' + appPath + '/data.json';
+var gdata = require('gulp-data');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var imagemin = require('gulp-imagemin');
+var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var inlineCss = require('gulp-inline-css');
+var html2pdf = require('gulp-html2pdf');
+var s3 = require('gulp-s3');
+
+var data = require(appPath + '/data.json');
 
 gulp.task('server', function () {
-    var connect = require('connect');
-    var serveStatic = require('serve-static');
-    var http = require('http');
-
-    var port = 8008;
-
     connect()
         .use(serveStatic(outputPath))
         .listen(port)
@@ -28,7 +35,6 @@ gulp.task('server', function () {
 });
 
 gulp.task('clean', function () {
-    var del = require('del');
     del.sync([
             outputPath + '/**/*',
             '!' + outputPath + '/.gitkeep'
@@ -36,7 +42,6 @@ gulp.task('clean', function () {
 });
 
 gulp.task('images', function() {
-    var imagemin = require('gulp-imagemin');
     return gulp
         .src(imagesPath)
         .pipe(imagemin({
@@ -46,8 +51,6 @@ gulp.task('images', function() {
 });
 
 gulp.task('styles', function () {
-    var stylus = require('gulp-stylus');
-    var concat = require('gulp-concat');
     return gulp
         .src(stylPath)
         .pipe(plumber())
@@ -58,13 +61,12 @@ gulp.task('styles', function () {
 });
 
 gulp.task('templates', function () {
-    var data = require(dataPath);
     data.isPDF = false;
 
     return gulp
         .src(tplPath + '/*.jade')
         .pipe(plumber())
-        .pipe(gulpData(data))
+        .pipe(gdata(data))
         .pipe(jade({
             pretty: true
         }))
@@ -72,15 +74,12 @@ gulp.task('templates', function () {
 });
 
 gulp.task('pdf', ['styles'], function () {
-    var inlineCss = require('gulp-inline-css');
-    var html2pdf = require('gulp-html2pdf');
-    var data = require(dataPath);
     data.isPDF = true;
 
     return gulp
         .src(tplPath + '/resume.jade')
         .pipe(plumber())
-        .pipe(gulpData(data))
+        .pipe(gdata(data))
         .pipe(jade({
             pretty: true
         }))
@@ -103,8 +102,6 @@ gulp.task('watch', function () {
 gulp.task('build', ['clean', 'images', 'styles', 'pdf', 'templates']);
 
 gulp.task('deploy', ['build', 'pdf'], function () {
-    var fs = require('fs');
-    var s3 = require('gulp-s3');
     var config = JSON.parse(fs.readFileSync('aws.json'));
     var options = {
         headers: {
